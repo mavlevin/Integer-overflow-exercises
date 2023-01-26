a91a833b42dd3b49fcf92774d08fef4db0743650
Guy Maverick Levin (Mav) <33753158+mavlevin@users.noreply.github.com>
2023-01-25 19:30:02 -0800

    Update compile_all.sh

--- compile_all.sh
+++ compile_all.sh
@@ -1,6 +1,6 @@
-gcc into-1cyclops.c -o into-1cyclops.c > /dev/null 2>&1
-gcc into-2walrus-tusks.c -o into-2walrus-tusks.c > /dev/null 2>&1
-gcc into-3tricycle.c -o into-3tricycle.c > /dev/null 2>&1
-gcc into-4horse-hooves.c -o into-4horse-hooves.c > /dev/null 2>&1
-gcc into-5devil-star.c -o into-5devil-star.c > /dev/null 2>&1
-gcc into-6two-triangles.c -o into-6two-triangles.c > /dev/null 2>&1
+gcc into-1cyclops.c -o into-1cyclops.c
+gcc into-2walrus-tusks.c -o into-2walrus-tusks.c
+gcc into-3tricycle.c -o into-3tricycle.c
+gcc into-4horse-hooves.c -o into-4horse-hooves.c
+gcc into-5devil-star.c -o into-5devil-star.c
+gcc into-6two-triangles.c -o into-6two-triangles.c
