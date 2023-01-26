56848f26ffd9ad4b65dd78c5490d38f85c74c5f6
Guy Maverick Levin (Mav) <33753158+mavlevin@users.noreply.github.com>
2023-01-25 19:26:36 -0800

    build script

--- compile_all.sh
+++ compile_all.sh
@@ -1,6 +1,6 @@
-gcc into-1cyclops.c -o into-1cyclops.c 2>/dev/null
-gcc into-2walrus-tusks.c -o into-2walrus-tusks.c 2>/dev/null
-gcc into-3tricycle.c -o into-3tricycle.c 2>/dev/null
-gcc into-4horse-hooves.c -o into-4horse-hooves.c 2>/dev/null
-gcc into-5devil-star.c -o into-5devil-star.c 2>/dev/null
-gcc into-6two-triangles.c -o into-6two-triangles.c 2>/dev/null
+gcc into-1cyclops.c -o into-1cyclops.c >/dev/null 2>&1
+gcc into-2walrus-tusks.c -o into-2walrus-tusks.c >/dev/null 2>&1
+gcc into-3tricycle.c -o into-3tricycle.c >/dev/null 2>&1
+gcc into-4horse-hooves.c -o into-4horse-hooves.c >/dev/null 2>&1
+gcc into-5devil-star.c -o into-5devil-star.c >/dev/null 2>&1
+gcc into-6two-triangles.c -o into-6two-triangles.c >/dev/null 2>&1
