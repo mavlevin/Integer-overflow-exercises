3471a3813a0ff46385fd3451ff048612b7565e66
Guy Maverick Levin (Mav) <33753158+mavlevin@users.noreply.github.com>
2023-01-25 19:24:50 -0800

    Add files via upload

---
+++ compile_all.sh
@@ -0,0 +1,6 @@
+gcc into-1cyclops.c -o into-1cyclops.c 2>/dev/null
+gcc into-2walrus-tusks.c -o into-2walrus-tusks.c 2>/dev/null
+gcc into-3tricycle.c -o into-3tricycle.c 2>/dev/null
+gcc into-4horse-hooves.c -o into-4horse-hooves.c 2>/dev/null
+gcc into-5devil-star.c -o into-5devil-star.c 2>/dev/null
+gcc into-6two-triangles.c -o into-6two-triangles.c 2>/dev/null
